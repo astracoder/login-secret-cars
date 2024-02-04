9c5318f3e3629f208bb72bdbc4d29e6da254c8e1
Drazic <cy4kqq@hotmail.com>
2024-02-04 11:25:43 -0300

    Finished validate inputs and buttons (disabled)

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -6,7 +6,7 @@ import { validateEmail, validatePassword } from '../../utils/Validators.js';
 import AppContainer from "../../components/AppContainer/AppContainer";
 
 const Login = () => {
-    const [, setLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
     const [form, setForm] = useState([]);
 
     const handleChange = e => {
@@ -23,8 +23,6 @@ const Login = () => {
         try {
             setLoading(true);
             console.log('Login feito com sucesso!');
-            setLoading(false);
-            console.log('Form está valido: ' + validatorInput());
         } catch(err) {
             console.log('Deu algum erro no login!');
         }
@@ -32,7 +30,7 @@ const Login = () => {
 
     return (
         <AppContainer>
-            <h1 className="text-white text-8xl font-bold mb-5">Secret Cars</h1>
+            <h1 className="text-white text-8xl text-center font-bold mb-5">Secret Cars</h1>
             <div className="flex justify-center items-center flex-col">
                 <h1 className="text-blue-200 text-4xl font-semibold mb-12">O melhor jogo de palavras</h1>
                 <h3 className="text-white/80 text-2xl font-light mb-5">Já tem <Link to="/register"><span className="underline cursor-pointer hover:text-blue-200">cadastro</span></Link>? Faça o seu login!</h3>
@@ -65,10 +63,19 @@ const Login = () => {
                     <a className="text-white hover:text-white/80 hover:underline text-xl font-light cursor-pointer">Recuperar senha &gt;</a>
                 </div>
                 <div className="flex justify-center items-center flex-col">
-                    <button 
-                        onClick={handleSubmit} 
-                        className="w-96 p-5 text-white text-xl bg-blue-500 hover:bg-blue-600 rounded-md mb-5">
-                    Entrar</button>
+                    {loading || !validatorInput() ? (
+                        <button 
+                            onClick={handleSubmit}
+                            disabled={true}
+                            className="w-96 p-5 text-white text-xl bg-blue-500 hover:bg-blue-600 opacity-50 rounded-md mb-5">
+                        Entrar</button>
+                    ) : (
+                        <button 
+                            onClick={handleSubmit}
+                            disabled={false}
+                            className="w-96 p-5 text-white text-xl bg-blue-500 hover:bg-blue-600 rounded-md mb-5">
+                        Entrar</button>
+                    )}
 
                     <Link to="/register">
                         <button className="w-96 p-5 text-white text-xl bg-white/10 border-2 border-white/20 hover:bg-white hover:text-gray-800 rounded-md">Registrar</button>
